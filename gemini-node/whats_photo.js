e6dcb87a38e2aeedc43e16b132ced8405d2e2ada
yoo16 <yohei.yoshikawa@gmail.com>
2024-09-18 09:01:09 +0900

    update

--- gemini-node/whats_photo.js
+++ gemini-node/whats_photo.js
@@ -1,13 +1,17 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const fs = require("fs");
 require('dotenv').config();
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 async function run() {
+    const prompt = "What's in this photo?";
+    console.log(prompt);
+
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const result = await model.generateContent([
-        "What is in this photo?",
+        prompt,
         {
             inlineData: {
                 data: Buffer.from(fs.readFileSync('./images/cappuccino.webp')).toString("base64"),
